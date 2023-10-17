e8614f7b459435ac86b96514aa2559d090bdf2a6
Luke Wheldale <lukewheldale@gmail.com>
2023-10-17 19:28:33 +1030

    feat: nodemon installed, command written

--- index.js
+++ index.js
@@ -4,12 +4,15 @@ const express = require("express");
 // make instace of server that we can customise and run
 const app = express();
 
+const HOST = process.env.HOST || "localhost";
+const PORT = process.env.PORT || 3000;
+
 // GET localhost:3000/
 app.get("/", (request, response) => {
 	response.send("Hello World");
 });
 
 // activate the server
-app.listen(3000, () => {
-	console.log("Server is running");
+app.listen(PORT, HOST, () => {
+	console.log("Server is running: " + PORT);
 });
