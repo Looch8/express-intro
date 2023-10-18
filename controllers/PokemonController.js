6f1f500e987c2d35c4a45aa0926a75a7338e4075
Luke Wheldale <lukewheldale@gmail.com>
2023-10-18 14:18:22 +1030

    fix: async route with fetch working

--- controllers/PokemonController.js
+++ controllers/PokemonController.js
@@ -4,9 +4,12 @@ const express = require("express");
 const router = express.Router();
 
 // export the router so it can be used by the rest of the app
-router.get("/", (request, response) => {
+router.get("/", async (request, response) => {
+	let result = await fetch("https://pokeapi.co/api/v2/pokemon/25");
+	let data = result.json();
 	response.json({
 		message: "Hello world from a router",
+		pokemonName: data.name,
 	});
 });
 
